30d6495acec870cfaf38b6860b517c8f15b4701e
Abhishek Kaushik <akaushik759@hotmail.com>
2017-11-07 20:24:45 +0530

    Initial Commit v-1 date-11/7/2017

---
+++ app/src/main/java/com/mvp/urbanpay/PayMoney.java
@@ -0,0 +1,13 @@
+package com.mvp.urbanpay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PayMoney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_money);
+    }
+}
